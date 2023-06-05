42499580e427bb9a9eab8e696d8c4f2bed4f377d
jcraggie <jcraggie@hotmail.com>
2023-06-05 18:42:23 -0500

    added LeslieModel to the project. Love Like Leslie!

---
+++ Learning-GH-2023/LeslieModel.swift
@@ -0,0 +1,16 @@
+//
+//  LeslieModel.swift
+//  Learning-GH-2023
+//
+//  Created by Jason Rogers on 6/5/23.
+//
+
+import Foundation
+
+// this is just testing github and adding new files
+// in memory of Leslie Nicole Rogers 1996-2023
+// she forever lives in our hears
+// never forgotten
+// I love you, Daughter.
+
+
